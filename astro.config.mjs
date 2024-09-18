fa4db9b64034963086707c373db047ead708e3c3
robertocarrazzoni <roberto.carrazzoni17@gmail.com>
2024-09-18 20:04:18 -0300

    Finished blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-blog1234.netlify.app/"
+  site: "https://astro-blog1234.netlify.app/",
+  integrations: [preact()]
 });
